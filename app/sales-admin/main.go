d41f9e8fa180c99f6795abb45b3fbc79b26c80e7
Rakhat <raha0264@gmail.com>
2021-06-07 01:54:18 +0600

    [lesson 25,26,27] 7.2, 7.3, 7.4 Auth [Completed]

---
+++ app/sales-admin/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"github.com/dgrijalva/jwt-go/v4"
+	"io/ioutil"
+	"log"
+	"os"
+	"time"
+)
+
+func main() {
+	//keygen()
+	tokengen()
+}
+
+func tokengen() {
+	privatePEM, err := ioutil.ReadFile("C:\\Users\\Rakhat\\go\\src\\goardanfinal\\private.pem")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	privateKey, err := jwt.ParseRSAPrivateKeyFromPEM(privatePEM)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Generating a token requires defining a set of claims. In this applications
+	// case, we only care about defining the subject and the user in question and
+	// the roles they have on the database. This token will expire in a year.
+	//
+	// iss (issuer): Issuer of the JWT
+	// sub (subject): Subject of the JWT (the user)
+	// aud (audience): Recipient for which the JWT is intended
+	// exp (expiration time): Time after which the JWT expires
+	// nbf (not before time): Time before which the JWT must not be accepted for processing
+	// iat (issued at time): Time at which the JWT was issued; can be used to determine age of the JWT
+	// jti (JWT ID): Unique identifier; can be used to prevent the JWT from being replayed (allows a token to be used only once)
+	claims := struct {
+		jwt.StandardClaims
+		Authorized []string
+	}{
+		StandardClaims: jwt.StandardClaims{
+			Issuer:    "service project",
+			Subject:   "123456789",
+			ExpiresAt: jwt.At(time.Now().Add(8760 * time.Hour)),
+			IssuedAt:  jwt.Now(),
+		},
+		Authorized: []string{"ADMIN"},
+	}
+
+	method := jwt.GetSigningMethod("RS256")
+	tkn := jwt.NewWithClaims(method, claims)
+	tkn.Header["kid"] = "sadasfsaf-132456-safasf"
+	str, err := tkn.SignedString(privateKey)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("-----BEGIN TOKEN-----\n%s\n-----END TOKEN-----\n", str)
+}
+
+func keygen() {
+
+	// Generate a new private key.
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Create a file for the private key information in PEM form.
+	privateFile, err := os.Create("private.pem")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer privateFile.Close()
+
+	// Construct a PEM block for the private key.
+	privateBlock := pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
+	}
+
+	// Write the private key to the private key file.
+	if err := pem.Encode(privateFile, &privateBlock); err != nil {
+		log.Fatalln(err)
+	}
+
+	// =====================================================================
+
+	// Marshal the public key from the private key to PKIX.
+	asn1Bytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Create a file for the public key information in PEM form.
+	publicFile, err := os.Create("public.pem")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer publicFile.Close()
+
+	// Construct a PEM block for the public key.
+	publicBlock := pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: asn1Bytes,
+	}
+
+	// Write the public key to the private key file.
+	if err := pem.Encode(publicFile, &publicBlock); err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("private and public key files generated")
+
+}
--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ go 1.15
 
 require (
 	github.com/ardanlabs/conf v1.4.0
+	github.com/dgrijalva/jwt-go/v4 v4.0.0-preview1
 	github.com/dimfeld/httptreemux/v5 v5.3.0
-	github.com/divan/expvarmon v0.0.0-20190204123027-8bf297f0fa5d // indirect
 	github.com/go-playground/locales v0.13.0
 	github.com/go-playground/universal-translator v0.17.0
 	github.com/google/uuid v1.2.0
